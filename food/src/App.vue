0046cb822c9fad9c45818ebb98ad2d324b234861
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-05-13 12:12:19 +0200

    Initial commit created project
    Add basic structure:
     three macro areas, variables with color palette, installed bootstrap

---
+++ food/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+
+    <header id="site_header"></header>
+    <!-- /#site_header -->
+
+    <main id="site_main"></main>
+    <!-- /#site_main -->
+
+    <footer id="site_footer"></footer>
+    <!-- /#site_footer -->
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/style.scss';
+</style>
